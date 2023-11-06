e5d7609f973ea8eebb472d401a2d9c310832c335
NaroPad7 <120739676+NaroPad7@users.noreply.github.com>
2023-11-05 20:15:45 -0600

    update

--- Services/MainService.cs
+++ Services/MainService.cs
@@ -1,5 +1,10 @@
 ﻿using ContextExample.Data;
 using System;
+using ContextExample.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Serialization;
 
 namespace ContextExample.Services;
 
@@ -16,13 +21,58 @@ public class MainService : IMainService
         _context = context;
     }
 
+    
+
     public void Invoke()
     {
+        string choice;
         // provide an option to the user to 
-        // 1. select by id
-        // 2. select by title 
-        // 3. find movie by title
+        do
+        {
+            Console.WriteLine("---Menu---");
+            Console.WriteLine("1. Get Movie by Id");
+            Console.WriteLine("2. Get movie by title");
+            Console.WriteLine("3. Find List of movies matching title");
+            Console.WriteLine("4.Exit");
+            Console.WriteLine("Enter a choice: ");
+            choice = Console.ReadLine();
+            // 1. select by id
+            if (choice == "1")
+            {
+                Console.WriteLine();
+                Console.WriteLine("What movie would you like to choose: ");
+                int movieChoice = Convert.ToInt32(Console.ReadLine());
+                var movie1 = _context.GetById((movieChoice));
+                Console.WriteLine($"Your movie is {movie1.Title}");
+
+            }
+            // 2. select by title 
+            else if (choice == "2")
+            {
+                Console.WriteLine("Enter a title to search for: ");
+                string searchTitle = Console.ReadLine();
+                var movieByTitle = _context.GetByTitle(searchTitle);
+                Console.WriteLine($"Matching movie is :{movieByTitle.Title}");
+
+
+            }
+            // 3. find movie by title
+            else if (choice == "3")
+            {
+                Console.WriteLine("Enter a partial title to search for: ");
+                string searchTitle = Console.ReadLine();
+                var matchingMovies = _context.FindMovie(searchTitle);
+                Console.WriteLine($"Movie titles found with that matches {matchingMovies}");
+
+            }
+        }
+        while (choice != "4");
+
+        
+       
         var movie = _context.GetById(1);
         Console.WriteLine($"Your movie is {movie.Title}");
+
+
     }
 }
